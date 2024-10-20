2a7035261ed56547d37235edb077a94bf0a4e497
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2024-10-19 21:50:58 -0500

    Initial commit

---
+++ test/s3-file-manager.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3FileManager from '../lib/s3-file-manager-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3-file-manager-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3FileManager.S3FileManagerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
